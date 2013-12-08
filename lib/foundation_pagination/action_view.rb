ae7459a55523054e91144f709ff049b94c917e2c
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-07 21:47:24 -0600

    use with Zurb Foundation

--- lib/foundation_pagination/action_view.rb
+++ lib/foundation_pagination/action_view.rb
@@ -2,7 +2,7 @@ require "will_paginate/view_helpers/action_view"
 require "foundation_pagination/foundation_renderer"
 
 module FoundationPagination
-  # A custom renderer class for WillPaginate that produces markup suitable for use with Twitter Bootstrap.
+  # A custom renderer class for WillPaginate that produces markup suitable for use with Zurb Foundation.
   class Rails < WillPaginate::ActionView::LinkRenderer
     include FoundationRenderer
   end
--- lib/foundation_pagination/sinatra.rb
+++ lib/foundation_pagination/sinatra.rb
@@ -2,7 +2,7 @@ require "will_paginate/view_helpers/sinatra"
 require "foundation_pagination/foundation_renderer"
 
 module FoundationPagination
-  # A custom renderer class for WillPaginate that produces markup suitable for use with Twitter Bootstrap.
+  # A custom renderer class for WillPaginate that produces markup suitable for use with Zurb Foundation.
   class Sinatra < WillPaginate::Sinatra::LinkRenderer
     include FoundationRenderer
   end
